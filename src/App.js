d557b944f5346bf1955cf782119942d3e2109b47
ElenaCiur <elena.ciuraite@gmail.com>
2019-03-09 18:47:46 +0200

    Inirial

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'lodash'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      results: {},
+      searchValue: ''
+    }
+  }
+  
+  async loadMovies(searchValue) {
+        
+    var newResults = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=${searchValue}&page=1&include_adult=false`)
+    
+    this.setState({
+      results: newResults.data.results
+    })
+  }
+  
+  render() {
+    return (
+      <div> 
+        <div 
+          style={{
+            backgroundColor: '#F66B27',
+            padding: '20px',
+            display: 'flex'
+          }}
+        >
+          <input
+            onChange={(event) => {
+              this.setState({ searchValue: event.target.value})
+              this.loadMovies(event.target.value)
+            }}
+            placeholder='Enter movie name'
+            style={{
+              backgroundColor: 'orange',
+              padding: '10px',
+              border: 'none',
+              width: '100%',
+              boxSizing: 'border-box',
+            }}
+          /> 
+          <button
+            onClick={() => this.loadMovies(this.state.searchValue)}
+            style={{
+              backgroundColor: 'white',
+              color: 'orange',
+              border: 'none',
+           }}
+          > 
+            Search
+          </button>
+        </div>
+        
+        {_.map(_.take(this.state.results, 8), (movie) => (
+          <div
+            style={{
+              backgroundColor: 'gray',
+              margin: '10px'
+            }}
+          >
+            <div>
+              {movie.title}
+            </div>
+            <div>
+              {movie.release_date}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
